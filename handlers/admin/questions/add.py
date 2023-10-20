d2da5f6251ecdf59e0b678421a26bb12f9111ef3
VladimirLakhtin <vllakhtin@gmail.com>
2023-10-20 19:07:24 +0300

    remove handler

--- handlers/admin/questions/add.py
+++ handlers/admin/questions/add.py
@@ -3,7 +3,7 @@ from aiogram import types as t
 from aiogram.fsm.context import FSMContext
 
 from handlers.admin.questions.states import CreateQuestionForm
-from handlers.funcs import create_question
+from handlers.funcs.questions import create_question
 from keyboards.admin import back_to_start_keyboard, start_keyboard
 from models.db import session_factory
 from text import admin as txt
@@ -11,20 +11,11 @@ from text import admin as txt
 router = Router()
 
 
-@router.message((F.from_user.username == "yummy_lvl") & (F.text == "Добавить вопрос"))
+@router.message(F.text == "Добавить вопрос")
 async def start_create_question_handler(message: t.Message, state: FSMContext):
     await state.set_state(CreateQuestionForm.question)
     keyboard = back_to_start_keyboard()
-    await message.answer(text=txt.CREATE_QUESTION,
-                         reply_markup=keyboard.as_markup(resize_keyboard=True))
-
-
-@router.message(F.text == 'Назад')
-async def cancel(message: t.Message, state: FSMContext):
-    await state.clear()
-    keyboard = start_keyboard()
-    await message.answer(text=txt.CANCEL_CREATE_QUESTION,
-                         reply_markup=keyboard.as_markup(resize_keyboard=True))
+    await message.answer(text=txt.CREATE_QUESTION, reply_markup=keyboard)
 
 
 @router.message(CreateQuestionForm.question)
@@ -40,6 +31,5 @@ async def process_answer_handler(message: t.Message, state: FSMContext):
     data = await state.get_data()
     await state.clear()
     await create_question(session_factory, data)
-    keyboard = start_keyboard()
-    await message.answer(txt.SUCCESS_CREATE_QUESTION,
-                         reply_markup=keyboard.as_markup(resize_keyboard=True))
+    keyboard = await start_keyboard()
+    await message.answer(txt.SUCCESS_CREATE_QUESTION, reply_markup=keyboard)
