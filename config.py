366e93f7bac2b270ac472df9a07d726e3eb36a56
VladimirLakhtin <vllakhtin@gmail.com>
2023-10-20 19:05:56 +0300

    add configurations

--- config.py
+++ config.py
@@ -5,7 +5,14 @@ from pathlib import Path
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 BASE_DIR = Path(__file__).parent
+MEDIA_DIR = BASE_DIR / "media"
+DOCUMENTS_DIR = MEDIA_DIR / "documents"
+CONULT_FORM = "https://forms.wix.com/f/6988831737677611262"
+
 DB_PATH = BASE_DIR / "db.sqlite3"
 SQLA_DB_URI = f"sqlite+aiosqlite:///{DB_PATH}"
+
+LATITUDE = 60.004934811312374
+LONGITUDE = 30.197177012066668
 
 logging.basicConfig(level=logging.INFO)
