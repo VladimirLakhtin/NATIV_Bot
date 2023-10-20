80e1cbf139e6c745db792b5985fe55549db9eb0a
VladimirLakhtin <vllakhtin@gmail.com>
2023-10-20 19:06:42 +0300

    connect routers

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,6 +1,6 @@
 from aiogram import Router
 
-from .users import router as user_router
+from .user import router as user_router
 from .admin import router as admin_router
 
 
--- handlers/users/__init__.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Router
-
-from .start import router as start_router
-from .questions import router as question_router
-
-router = Router()
-router.include_routers(start_router, question_router)
