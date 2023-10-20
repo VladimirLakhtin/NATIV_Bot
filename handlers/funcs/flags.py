330b70bee6ce2b6ae15be247a9b1def3290d30c1
VladimirLakhtin <vllakhtin@gmail.com>
2023-10-20 19:08:13 +0300

    add flag funcs

---
+++ handlers/funcs/flags.py
@@ -0,0 +1,23 @@
+from sqlalchemy import select, Result
+from sqlalchemy.ext.asyncio import async_sessionmaker
+
+from models import Flag
+
+
+async def check_consultation_available(session_factory: async_sessionmaker) -> bool:
+    async with session_factory() as session:
+        stmt = select(Flag).filter(Flag.name == 'consultation')
+        result: Result = await session.execute(stmt)
+        flag = result.scalar()
+    return flag.value
+
+
+async def change_consultation_available(session_factory: async_sessionmaker) -> bool:
+    async with session_factory() as session:
+        stmt = select(Flag).filter(Flag.name == 'consultation')
+        result: Result = await session.execute(stmt)
+        flag = result.scalar()
+        flag.value = not flag.value
+        session.add(flag)
+        await session.commit()
+        return flag.value
